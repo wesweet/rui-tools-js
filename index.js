b4c613d85877d3a6263d4b5c06dfb9d421c44e3d
panrui <panrui@mujoy.com>
2021-03-17 18:25:37 +0800

    添加数组扁平化方法

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Description: 入口文件
  * @Author: panrui
  * @Date: 2021-03-15 10:14:11
- * @LastEditTime: 2021-03-15 10:26:39
+ * @LastEditTime: 2021-03-17 18:22:46
  * @LastEditors: panrui
  * 不忘初心,不负梦想
  */
@@ -20,5 +20,14 @@ export default {
       reader.onload = () => resolve(reader.result);
       reader.onerror = error => reject(error);
     });
+  },
+  /**
+   * 函数扁平化
+   * @param {*} array 
+   */
+  flatten(array) {
+    return array.reduce(function (pre, item) {
+      return pre.concat(Array.isArray(item) ? flatten(item) : item);
+    }, [])
   }
 }
