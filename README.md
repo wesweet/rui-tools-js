bead103c73f5b22f00d0012e6f9dfbcc0a07c203
panrui <panrui@mujoy.com>
2021-06-17 17:03:02 +0800

    feat:新增 删除对象数组中 指定项方法

--- README.md
+++ README.md
@@ -11,21 +11,91 @@ JavaScripts 常用方法工具库
 #### 图片转成 base
 
 传入图片对象,返回一个 promise 对象,在成功函数获取 base64
+
 ```js
-getBase64(file)
+getBase64(file);
 ```
 
 #### 数组扁平化
 
 将一个多维数组转换成一维数组
+
 ```js
 // [1, [2, 3, [4, 5]]]  ------>    [1, 2, 3, 4, 5]
-flatten(array)
+flatten(array);
 ```
 
-#### 普通数组转成树形结构数组
+#### 一维对象数组转成树形结构数组
+
 ```js
-// 
-treeData(source, id, parentId, children)
+let dataSource = [
+  {
+    id: 1,
+    pid: 0,
+    name: "一级菜单A",
+  },
+  {
+    id: 2,
+    pid: 0,
+    name: "一级菜单B",
+  },
+  {
+    id: 3,
+    pid: 0,
+    name: "一级菜单C",
+  },
+  {
+    id: 4,
+    pid: 1,
+    name: "二级菜单A-A",
+  },
+  {
+    id: 5,
+    pid: 1,
+    name: "二级菜单A-B",
+  },
+  {
+    id: 6,
+    pid: 2,
+    name: "二级菜单B-B",
+  },
+  {
+    id: 7,
+    pid: 2,
+    name: "二级菜单B-C",
+  },
+  {
+    id: 8,
+    pid: 6,
+    name: "三级菜单B-B-A",
+  },
+  {
+    id: 9,
+    pid: 6,
+    name: "三级菜单B-B-B",
+  },
+];
 ```
 
+#### 删除对象数组中 指定属性的值为 value 的项
+
+```js
+[
+  {
+    id: 1,
+    value: 2,
+    children: [
+      {
+        id: 2,
+        value: 3,
+        children: [
+          {
+            id: 3,
+            value: 4,
+          },
+        ],
+      },
+    ],
+  },
+];
+```
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Description: 入口文件
  * @Author: panrui
  * @Date: 2021-03-15 10:14:11
- * @LastEditTime: 2021-06-03 14:59:48
+ * @LastEditTime: 2021-06-17 16:59:20
  * @LastEditors: panrui
  * 不忘初心,不负梦想
  */
@@ -31,19 +31,34 @@ export default {
     }, [])
   },
   /**
-   * 根节点需要保证为0
-   * 将单维数组转换成树形结构
-   * @param {*} source 元数组
-   * @param {*} id 节点id
-   * @param {*} parentId 父节点id
-   * @param {*} children 挂载在父节点的字段
+   * 添加一个对象作为属性，如果后续该对象添加了属性，那么也会影响之前的
+   * 将一维对象数组转换成树形结构对象数组
+   * @param {*} source 一维对象数组
+   * @param {*} parentId 数组项 父节点字段属性
+   * @param {*} id 数组项 自己节点字段属性
+   * @param {*} children 数组项 子节点字段属性
    */
   treeData(source, id, parentId, children) {
     let cloneData = JSON.parse(JSON.stringify(source)) //克隆数组
     return cloneData.filter(father => {
-        let branchArr = cloneData.filter(child => father[id] == child[parentId]);
-        branchArr.length > 0 ? father[children] = branchArr : ''
-        return father[parentId] == 0 
+      let branchArr = cloneData.filter(child => father[id] == child[parentId]);
+      branchArr.length > 0 ? father[children] = branchArr : ''
+      return father[parentId] == 0 // 最顶层节点的父节点值 修改成你自己的值
+    })
+  },
+  /**
+   * 删除对象数组中 指定属性的值为 value的项
+   * @param {*} data 
+   * @param {*} value 
+   */
+  findItem(data, value) {
+    data.forEach((item, index) => {
+      if (item.key == value) {
+        data.splice(index, 1)
+      }
+      if (item.children.length) {
+        return this.findItem(item.children, value)
+      }
     })
   }
 }
